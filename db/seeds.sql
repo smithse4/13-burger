6363067bbf10a4dae227d237b0ef6a926d72822e
Stephanie smith <smithse4@gmail.com>
2020-04-09 19:01:02 -0400

    added schema

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger with onions', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
